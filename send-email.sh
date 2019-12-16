ce0d41532e622cc583638fb09c1811838a1e7ac8
edvcel <edvcel@gmail.com>
2019-12-16 19:47:51 +0200

    Created the main shell script

---
+++ send-email.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Input the recipient: " address
+read -p "What is the subject of the email? " subject
+read -p "Input the body of the email: " body
+
+echo -e "Subject: $subject\r\n\r\n$body" |msmtp --from=default -t $address
+
+echo "The email has been sent successfully!"
