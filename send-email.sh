f7bbec4dd297446ff33530c122a4b91e7e217e18
edvcel <edvcel@gmail.com>
2019-12-16 20:14:44 +0200

    Updated send-email.sh

--- send-email.sh
+++ send-email.sh
@@ -4,6 +4,6 @@ read -p "Input the recipient: " address
 read -p "What is the subject of the email? " subject
 read -p "Input the body of the email: " body
 
-echo -e "Subject: $subject\r\n\r\n$body" |msmtp --from=default -t $address
+echo -e "To: @domain.com\nFrom: @gmail.com\nSubject: $subject\n$body" |msmtp --from=default -t $address
 
 echo "The email has been sent successfully!"
